cd5297edcb2140feac3a21b736883828b3820bac
guille garcia <ggarciacardiel@gmail.com>
2024-06-14 18:43:53 +0200

    updates slider background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import HamburgerMenu from "@/components/HamburgerMenu";
+import Image from "next/image"; // Import Image component from next/image
+import tremendologo from "../assets/tremendologo.png"; // Adjust the path as per your actual file location
 
 const NavigationFooter = dynamic(() => import("@/components/NavigationFooter"), {
   ssr: false,
@@ -19,14 +21,13 @@ export default function Home() {
       <main className="min-h-screen relative overflow-hidden w-full">
         <section className="fixed inset-0 min-h-screen w-full bg-transparent flex flex-col items-center justify-center p-8 md:p-12 z-[20] text-center text-white">
           <HamburgerMenu />
-          <Link href="/" className="text-[#FFFFFF] font-dancing text-[40px] leading-none md:text-[100px] my-4 md:my-8">
-            Tremendo
-          </Link>
+          {/* Use Image component instead of img tag */}
+          <div className="my-4 md:my-8">
+            <Image src={tremendologo} alt="Tremendo Logo" width={400} height={200} />
+          </div>
 
           <div className="flex flex-col items-center">
-            <h1 className="font-medium text-3xl sm:text-6xl md:text-[90px] lg:text-[80px] leading-none text-white mb-10 md:mb-16">
-              Sant Cugat
-            </h1>
+            
 
             <div className="navigation-footer">
               <NavigationFooter />
