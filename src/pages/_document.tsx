6acc9ca17c0e391b578d90047d3b160fbf078b67
guille garcia <ggarciacardiel@gmail.com>
2024-06-15 15:20:29 +0200

    wip language locale

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { useTranslation } from 'next-i18next';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps, locale: ctx.query.locale || 'ca' }; // Default to 'ca' if no locale is provided
+  }
+
   render() {
+    const { locale } = this.props as any; // Use locale from props
+
     return (
-      <Html lang="en">
+      <Html lang={locale}>
         <Head>
           {/* Preconnect to Google Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
