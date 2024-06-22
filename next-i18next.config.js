755ff214de40c037cf99f13d541318a8092b34f6
guille garcia <ggarciacardiel@gmail.com>
2024-06-22 16:12:00 +0200

    adds absolute path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,14 +1,17 @@
 "use strict";
+const path = require('path');
 Object.defineProperty(exports, "__esModule", { value: true });
+
 var nextI18NextConfig = {
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'es', 'ca'],
         localeDetection: false
     },
-    localePath: 'public/locales',
+    localePath: path.resolve('./public/locales'),
     ns: ['common'],
     reloadOnPrerender: process.env.NODE_ENV === 'development',
     debug: true,
 };
+
 exports.default = nextI18NextConfig;
